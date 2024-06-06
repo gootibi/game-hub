0a3ca8d87a1f709cea22d57f23eb58f52d965fdb
gootibi <gootibi@gmail.com>
2024-06-06 22:30:18 +0200

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import { Button, Heading, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -20,17 +20,20 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     }
 
     return (
-        <List>
-            {data.map(genre =>
-                <ListItem key={genre.id} paddingY='5px'>
-                    <HStack>
-                        <Image boxSize={"32px"} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                        <Button fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>)}
-        </List>
+        <>
+            <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+            <List>
+                {data.map(genre =>
+                    <ListItem key={genre.id} paddingY='5px'>
+                        <HStack spacing={3}>
+                            <Image boxSize={"32px"} borderRadius={8} objectFit='cover' src={getCroppedImageUrl(genre.image_background)} />
+                            <Button whiteSpace='normal' textAlign='left' fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>
+                                {genre.name}
+                            </Button>
+                        </HStack>
+                    </ListItem>)}
+            </List>
+        </>
     )
 }
 
